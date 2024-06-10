4182c561fa0a17c924013f828bedc0b204fc354e
SantiagoWainerr <swainer@itba.edu.ar>
2024-06-10 07:58:51 -0300

    Comentarios main.c

--- TP12_RPI/src/main.c
+++ TP12_RPI/src/main.c
@@ -2,13 +2,17 @@
 #include "getData.h"
 #include "hardware.h"
 int main (void){
+	// Inicializamos los gpios como salida
 	inicialization();
 	int c = DATO_ERR;
+
+	// Llamamos a la función getData() y evaluamos que el caracter ingresado no sea una 'q'. Si es una 'q', entonces el programa debe terminar.
 	while(c != 'q'){
 		printf("Ingrese la operación que quiere realizar: ");
 		while ((c = getData()) && c == DATO_ERR){
 			printf("Dato inválido. Ingrese nuevamente: ");
 		}
+		// Utilizamos un switch para evaluar el caracter que se ingresó y en base al caracter, que se realice una tarea específica. 
 		switch(c){
 			case 't':				// TOGGLE_ALL
 				toggle();
