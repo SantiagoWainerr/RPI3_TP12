b4050f1917be7124351094e2805c9cdd7391fd53
SantiagoWainerr <swainer@itba.edu.ar>
2024-06-10 12:19:09 -0300

    Actualizar hardware.c

--- TP12_RPI/src/hardware.c
+++ TP12_RPI/src/hardware.c
@@ -7,7 +7,7 @@
 
 FILE * archivo;
 
-char * mapeo[8] = {"17", "4", "19", "23", "24", "25", "22", "27"};
+char * mapeo[8] = {"17", "4", "18", "23", "24", "25", "22", "27"};
 
 void inicialization (void){
 	char numPin[TAM_MAX];
